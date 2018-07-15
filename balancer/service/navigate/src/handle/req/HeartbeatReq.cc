7a02ff6b47c7b9e12b70fd8c17006fe3f1d83a12
lulu <757533566@qq.com>
2018-07-15 22:19:26 +0800

    modify

--- balancer/service/navigate/src/handle/req/HeartbeatReq.cc
+++ balancer/service/navigate/src/handle/req/HeartbeatReq.cc
@@ -36,6 +36,21 @@ void HeartbeatReq::handle(const center::CenterMsg& msg)
 		return;
 	}
 
+	_proc._owner.update_owner_hb_time(req.level(), req.state());
+	if(req.conf_json().size() > 0)
+	{
+		// ¸üÐÂÅäÖÃ
+		ServiceConfig sc;
+		std::string err = sc.json_to_map(req.conf_json());
+		if(err.empty())
+		{
+			_proc._sc.assign(sc);
+
+
+			_proc._owner.update_owner(req.level(), req.conf_update_time(), _packet_ptr->_msg_seq_id);
+		}
+	}
+
 	PacketPtr packet_ptr_rsp(new Packet(_packet_ptr->_from_service_id, 0, 0, 0, 0, _packet_ptr->_msg_seq_id));
 	CenterStack::HeartbeatRsp(packet_ptr_rsp->_body,
 		common::SUCCESS,
@@ -43,8 +58,8 @@ void HeartbeatReq::handle(const center::CenterMsg& msg)
 		req.level(),
 		req.service_id(),
 		req.proc_id(),
-		req.conf_update_time(),
-		::time(nullptr));
+		_proc._owner._conf_update_time,
+		_proc._owner._expire_second);
 
 	_proc._tcp_server.send_msg(_conn, packet_ptr_rsp);
 }
