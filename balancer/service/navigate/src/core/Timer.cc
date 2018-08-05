b55643142d9b00f95c1e16e22f24f2ca1ea2a7f9
lulu <757533566@qq.com>
2018-08-05 16:46:01 +0800

    去掉时间，怕不准

--- balancer/service/navigate/src/core/Timer.cc
+++ balancer/service/navigate/src/core/Timer.cc
@@ -1,7 +1,6 @@
 #include "Timer.h"
 #include "log/Log.h"
 #include "Proc.h"
-#include "tool/Util.h"
 
 Timer::Timer(Proc& proc) : _proc(proc)
 {
@@ -15,7 +14,5 @@ Timer::~Timer()
 
 void Timer::check_timeout()
 {
-	unsigned long long now_us = Util::get_us();	
-
 	_proc.check_flag();
 }
