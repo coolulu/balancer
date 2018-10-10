cc81e81c5382a66d2bca407d247810fc2fac0e7c
lulu <757533566@qq.com>
2018-10-10 22:54:21 +0800

    定时器增加put_load.check_sync_gap

--- balancer/service/gate/src/core/Timer.cc
+++ balancer/service/gate/src/core/Timer.cc
@@ -16,5 +16,6 @@ void Timer::check_timeout()
 {
 	_proc.check_flag();
 	_proc._tcp_client_pool.check_idle();
-	_proc._task_msg_pool.check_timeout();	
+	_proc._task_msg_pool.check_timeout();
+	_proc._put_load.check_sync_gap();
 }
