c1e502cd330b59ab2757f849e00d4c579ecd41d5
lulu <757533566@qq.com>
2018-11-04 19:03:29 +0800

    test

--- balancer/test/proto/src/codec.py
+++ balancer/test/proto/src/codec.py
@@ -7,31 +7,31 @@ def test():
 
     p = packet.Packet()
 
-    p.data = b'1234567810'
-    print(len(p.data))
+    p._data = b'1234567810'
+    print(len(p._data))
 
-    stream = struct.pack('!IIHHHIIIQQBBIII' + str(len(p.data)) + 'sI',
-                         p.header,
-                         p.len,
-                         p.version,
-                         p.from_service_id,
-                         p.to_service_id,
-                         p.to_proc_id,
-                         p.app_id,
-                         p.app_version,
-                         p.conn_seq_id,
-                         p.msg_seq_id,
-                         p.data_format,
-                         p.reserve_field_0,
-                         p.reserve_field_1,
-                         p.reserve_field_2,
-                         p.reserve_field_3,
-                         p.data,
-                         p.check_sum)
+    stream = struct.pack('!IIHHHIIIQQBBIII' + str(len(p._data)) + 'sI',
+                         p._header,
+                         p._len,
+                         p._version,
+                         p._from_service_id,
+                         p._to_service_id,
+                         p._to_proc_id,
+                         p._app_id,
+                         p._app_version,
+                         p._conn_seq_id,
+                         p._msg_seq_id,
+                         p._data_format,
+                         p._reserve_field_0,
+                         p._reserve_field_1,
+                         p._reserve_field_2,
+                         p._reserve_field_3,
+                         p._data,
+                         p._check_sum)
     print(len(stream))
     print(stream)
 
-    ss = struct.pack('!' + str(len(p.data)) + 's', p.data)
+    ss = struct.pack('!' + str(len(p._data)) + 's', p._data)
     print(len(ss))
     print(ss)
 
--- balancer/test/proto/src/packet.py
+++ balancer/test/proto/src/packet.py
@@ -8,24 +8,24 @@ class Packet:
     k_header = 0x00000000
 
     def __init__(self):
-        self.header = 0
-        self.len = 0
-        self.version = 0
-        self.from_service_id = 0
-        self.to_service_id = 0
-        self.to_proc_id = 0
-        self.app_id = 0
-        self.app_version = 0
-        self.conn_seq_id = 0
-        self.msg_seq_id = 0
-        self.data_format = 0
-        self.reserve_field_0 = 0
-        self.reserve_field_1 = 0
-        self.reserve_field_2 = 0
-        self.reserve_field_3 = 0
-        self.data = b''
-        self.check_sum = 0
+        self._header = 0
+        self._len = 0
+        self._version = 0
+        self._from_service_id = 0
+        self._to_service_id = 0
+        self._to_proc_id = 0
+        self._app_id = 0
+        self._app_version = 0
+        self._conn_seq_id = 0
+        self._msg_seq_id = 0
+        self._data_format = 0
+        self._reserve_field_0 = 0
+        self._reserve_field_1 = 0
+        self._reserve_field_2 = 0
+        self._reserve_field_3 = 0
+        self._data = b''
+        self._check_sum = 0
 
-        self.data_pb2 = data_pb2.Body()
+        self._body = data_pb2.Body()
 
 
