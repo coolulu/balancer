e2a7af3d4fe112f5ecf0df63603cdda3cf8c1a97
lulu <757533566@qq.com>
2018-11-06 20:26:50 +0800

    update

--- balancer/service/proxy/bin/proxy.py
+++ balancer/service/proxy/bin/proxy.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
 from main import main
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
--- balancer/service/proxy/src/main.py
+++ balancer/service/proxy/src/main.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import getopt
+
 from config import config
 from log import log
 
 
+def main(argv):
 
-def main():
+    config_file = ''
+
+    try:
+        opts, args = getopt.getopt(argv, 'f:', ['config_file='])
+    except getopt.GetoptError:
+        print('python bin/proxy -f conf/proxy_0.json')
+        sys.exit(-1)
+
+    for opt, arg in opts:
+        if opt in ("-i", "--ifile"):
+            config_file = arg
+
 
     with open('./conf/proxy_0.json') as fd:
         str = fd.read(1024 * 1024)
