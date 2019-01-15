1f31aca57476d8e4d01867cf8cdcb793175ae2fa
lulu <757533566@qq.com>
2019-01-15 11:39:26 +0800

    zlib.adler32(stream_middle, 0)包校验

--- balancer/service/proxy/src/core/codec.py
+++ balancer/service/proxy/src/core/codec.py
@@ -1,5 +1,7 @@
 
 import struct
+import zlib
+
 from core import packet
 
 
@@ -39,3 +41,26 @@ def test():
 
 test()
 
+
+def stream_encode(pkt):
+    stream_head = struct.pack('!I', pkt.header)
+    stream_middle = struct.pack('!IHHHIIIQQBBIII' + str(len(pkt.data)) + 's',
+                                pkt.len,
+                                pkt.version,
+                                pkt.from_service_id,
+                                pkt.to_service_id,
+                                pkt.to_proc_id,
+                                pkt.app_id,
+                                pkt.app_version,
+                                pkt.conn_seq_id,
+                                pkt.msg_seq_id,
+                                pkt.data_format,
+                                pkt.reserve_field_0,
+                                pkt.reserve_field_1,
+                                pkt.reserve_field_2,
+                                pkt.reserve_field_3,
+                                pkt.data)
+    stream_check_sum = zlib.adler32(stream_middle, 0)
+    stream_end = struct.pack('!I', stream_check_sum)
+    stream = stream_head + stream_check_sum + stream_end
+    return stream
